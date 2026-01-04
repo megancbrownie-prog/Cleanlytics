14c9072ed92e9cc6b69a0b0c073eadc6248e18a8
megancbrownie-prog <megancbrownie@gmail.com>
2026-01-03 23:05:47 -0500

    Update Streamlit app introduction text

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,5 +2,4 @@ import streamlit as st
 
 st.title("🎈 My new app")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://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
